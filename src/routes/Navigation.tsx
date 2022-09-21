281883c5ab1bce20d51737cd0cb2903031d812db
glopez <glopez@techgenies.com>
2022-09-21 14:28:23 -0500

    LazyLoad basico listo!

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,50 +1,47 @@
+import { Suspense } from 'react'
 import { BrowserRouter, Navigate } from 'react-router-dom'
 import { Routes, NavLink, Route } from 'react-router-dom'
+
+import { routes } from '../01-lazyload/routes/routes'
 
 import logo from '../logo.svg'
 
 const Navigation = () => {
 	return (
-		<BrowserRouter>
+		<Suspense fallback={ <span>Loading...</span>}>
+			<BrowserRouter>
 			<div className='main-layout'>
 				<nav>
 					<img src={logo} alt='React logo' />
 
 					<ul>
-						<li>
-							<NavLink
-								to={'/'}
-								className={({ isActive }) => (isActive ? 'nav-active' : '')}>
-								Home
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to={'/about'}
-								className={({ isActive }) => (isActive ? 'nav-active' : '')}>
-								About
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to={'/users'}
-								className={({ isActive }) => (isActive ? 'nav-active' : '')}>
-								Users
-							</NavLink>
-						</li>
+						{
+							routes.map(({ to, name }) => (
+								<li key={to}>
+									<NavLink
+										to={to}
+										className={({ isActive }) => (isActive ? 'nav-active' : '')}>
+										{name}
+									</NavLink>
+								</li>
+							))
+						}
 					</ul>
 				</nav>
 
 				<Routes>
-					<Route path='about' element={<h1>About</h1>}></Route>
-					<Route path='users' element={<h1>Users</h1>}></Route>
-					<Route
-						path='/home'
-						element={<h1 className='{({isActive?})}'>Home</h1>}></Route>
-					<Route path='/*' element={<Navigate to='/home' />}></Route>
+					{
+						routes.map(({ path, Component }) => (
+							<Route key={path} path={path} element={<Component />} />
+						))
+					}
+
+					<Route path='/*' element={<Navigate to={routes[0].to} replace />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
+		</Suspense>
+		
 	)
 }
 
